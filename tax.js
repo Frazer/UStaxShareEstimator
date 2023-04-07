260996c9340db179bf83301aae11e0eb1619b6e6
Frazer Kirkman <frazerkirkman@gmail.com>
2023-04-07 03:47:43 -0700

    Tax share calculator

---
+++ tax.js
@@ -0,0 +1,276 @@
+let brackets = { 2023: {self : [
+    {min:0, max:11000, percent:10, amount:11000},
+    {min:11000, max:44725, percent:12, amount:44725-11000},
+    {min:44725, max:95375, percent:22, amount:95375-44725},
+    {min:95375, max:182100, percent:24, amount:182100-95375},
+    {min:182100, max:231250, percent:32, amount:231250-182100},
+    {min:231250, max:578125, percent:35, amount:578125-231250},
+    {min:578125, max:Infinity, percent:37, amount:Infinity},
+    ], joint:[
+    {min:0, max:22000, percent:10, amount:22000},
+    {min:22000, max:89450, percent:12, amount:89450-22000},
+    {min:89450, max:190750, percent:22, amount:190750-89450},
+    {min:190750, max:364200, percent:24, amount:364200-190750},
+    {min:364200, max:462500, percent:32, amount:462500-364200},
+    {min:462500, max:693750, percent:35, amount:693750-462500},
+    {min:693750, max:Infinity, percent:37, amount:Infinity},
+]},
+2022: {self : [
+    {min:0, max:10275, percent:10, amount:10275},
+    {min:10275, max:41775, percent:12, amount:41775-10275},
+    {min:41775, max:89075, percent:22, amount:89075-41775},
+    {min:89075, max:170050, percent:24, amount:170050-89075},
+    {min:170050, max:215950, percent:32, amount:215950-170050},
+    {min:215950, max:539900, percent:35, amount:539900-215950},
+    {min:539900, max:Infinity, percent:37, amount:Infinity},
+    ], joint: [
+    {min:0, max:20550, percent:10, amount:20550},
+    {min:20550, max:83550, percent:12, amount:83550-20550},
+    {min:83550, max:178150, percent:22, amount:178150-83550},
+    {min:178150, max:340100, percent:24, amount:340100-178150},
+    {min:340100, max:431900, percent:32, amount:431900-340100},
+    {min:431900, max:647850, percent:35, amount:647850-431900},
+    {min:647850, max:Infinity, percent:37, amount:Infinity},
+]},
+2021: {self : [
+    {min:0, max:9950, percent:10, amount:9950},
+    {min:9950, max:40525, percent:12, amount:40525-9950},
+    {min:40525, max:86375, percent:22, amount:86375-40525},
+    {min:86375, max:164925, percent:24, amount:164925-86375},
+    {min:164925, max:209425, percent:32, amount:209425-164925},
+    {min:209425, max:523600, percent:35, amount:523600-209425},
+    {min:523600, max:Infinity, percent:37, amount:Infinity},
+    ], joint:[
+    {min:0, max:19900, percent:10, amount:19900},
+    {min:19900, max:81050, percent:12, amount:81050-19900},
+    {min:81050, max:172750, percent:22, amount:172750-81050},
+    {min:172750, max:329850, percent:24, amount:329850-172750},
+    {min:329850, max:418850, percent:32, amount:418850-329850},
+    {min:418850, max:628300, percent:35, amount:628300-418850},
+    {min:628300, max:Infinity, percent:37, amount:Infinity},
+]},
+2020: {self : [
+    {min:0, max:9875, percent:10, amount:9875},
+    {min:9875, max:40125, percent:12, amount:40125-9875},
+    {min:40125, max:85525, percent:22, amount:85525-40125},
+    {min:85525, max:163300, percent:24, amount:163300-85525},
+    {min:163300, max:207350, percent:32, amount:207350-163300},
+    {min:207350, max:518400, percent:35, amount:518400-207350},
+    {min:518400, max:Infinity, percent:37, amount:Infinity},
+    ], joint:[
+    {min:0, max:19750, percent:10, amount:19750},
+    {min:19750, max:80250, percent:12, amount:80250-19750},
+    {min:80250, max:171050, percent:22, amount:171050-80250},
+    {min:171050, max:326600, percent:24, amount:326600-171050},
+    {min:326600, max:414700, percent:32, amount:414700-326600},
+    {min:414700, max:622050, percent:35, amount:622050-414700},
+    {min:622050, max:Infinity, percent:37, amount:Infinity},
+]},
+2019: {self : [
+    {min:0, max:9700, percent:10, amount:9700},
+    {min:9700, max:39475, percent:12, amount:39475-9700},
+    {min:39475, max:84200, percent:22, amount:84200-39475},
+    {min:84200, max:160725, percent:24, amount:160725-84200},
+    {min:160725, max:204100, percent:32, amount:204100-160725},
+    {min:204100, max:510300, percent:35, amount:510300-204100},
+    {min:510300, max:Infinity, percent:37, amount:Infinity},
+    ], joint:[
+    {min:0, max:19400, percent:10, amount:19400},
+    {min:19400, max:78950, percent:12, amount:78950-19400},
+    {min:78950, max:168400, percent:22, amount:168400-78950},
+    {min:168400, max:321450, percent:24, amount:321450-168400},
+    {min:321450, max:408200, percent:32, amount:408200-321450},
+    {min:408200, max:612350, percent:35, amount:612350-408200},
+    {min:612350, max:Infinity, percent:37, amount:Infinity},
+]},
+2018: {self : [
+    {min:0, max:9525, percent:10, amount:9525},
+    {min:9525, max:38700, percent:12, amount:38700-9525},
+    {min:38700, max:82500, percent:22, amount:82500-38700},
+    {min:82500, max:157500, percent:24, amount:157500-82500},
+    {min:157500, max:200000, percent:32, amount:200000-157500},
+    {min:200000, max:500000, percent:35, amount:500000-200000},
+    {min:500000, max:Infinity, percent:37, amount:Infinity},
+    ], joint:[
+    {min:0, max:19050, percent:10, amount:19050},
+    {min:19050, max:77400, percent:12, amount:77400-19050},
+    {min:77400, max:165000, percent:22, amount:165000-77400},
+    {min:165000, max:315000, percent:24, amount:315000-165000},
+    {min:315000, max:400000, percent:32, amount:400000-315000},
+    {min:400000, max:600000, percent:35, amount:600000-400000},
+    {min:600000, max:Infinity, percent:37, amount:Infinity},
+]},
+};
+
+let calcBrack = (amount, percent, arr, leftover)=>{
+    let use = Math.min(amount,leftover);
+    arr.push(use*percent/100);
+    return {tax: use*percent/100, left:leftover - use};
+}
+
+let sum = (arr)=>{
+    let tot = 0;
+    arr.forEach(v=>{
+        tot+=v;
+    })
+    return tot;
+}
+
+createRow = (percent, ...taxes)=>{
+    let row = document.createElement('tr');
+    let percentCell = document.createElement('td');
+    percentCell.innerHTML = percent+"%";
+    row.appendChild(percentCell);
+    for(const t of taxes){
+        let taxCell = document.createElement('td');
+        taxCell.innerHTML = "$"+t;
+        taxCell.title = percent+"% of $"+(t/percent*100);
+        row.appendChild(taxCell);
+    }
+    return row;
+}
+
+createBracketsRow = (bracket, jointBracket)=>{
+    let row = document.createElement('tr');
+    let percentCell = document.createElement('td');
+    percentCell.innerHTML = bracket.percent+"%";
+    row.appendChild(percentCell);
+    let taxCell = document.createElement('td');
+    taxCell.innerHTML = "$"+bracket.min;
+    if(bracket.max!=Infinity){
+        taxCell.innerHTML += " - $"+bracket.max;
+        taxCell.title = "$"+(bracket.max-bracket.min) + " in this bracket";
+    }else{
+        taxCell.innerHTML += " +";
+    }
+    row.appendChild(taxCell);
+    let jointTaxCell = document.createElement('td');
+    jointTaxCell.innerHTML = "$"+jointBracket.min;
+    if(jointBracket.max!=Infinity){
+        jointTaxCell.innerHTML += " - $"+jointBracket.max;
+    }else{
+        jointTaxCell.innerHTML += " +";
+    }
+    row.appendChild(jointTaxCell);
+    return row;
+};
+
+let person1 = document.getElementById('person1');
+let person2 = document.getElementById('person2');
+let person1Gross = document.getElementById('person1Gross');
+let person2Gross = document.getElementById('person2Gross');
+let person1Heading = document.querySelectorAll('.person1Heading');
+let person2Heading = document.querySelectorAll('.person2Heading');
+let fileSeperate = document.getElementById('fileSeperate');
+let fileJoint = document.getElementById('fileJoint');
+let p1Tot = document.getElementById('p1Tot');
+let p2Tot = document.getElementById('p2Tot');
+let slider = document.getElementById('slider');
+let TotJoint = document.getElementById('TotJoint');
+let p1TotJoint = document.getElementById('p1TotJoint');
+let p2TotJoint = document.getElementById('p2TotJoint');
+let bracketsTable = document.getElementById('bracketsTable');
+let year = document.getElementById('year');
+let pageTitle = document.querySelector('title');
+
+let calculate = ()=>{
+    let selectedYearBrackets = brackets[year.value];
+    pageTitle.innerHTML = "Tax Calculator - "+year.value;
+
+    for(var i = 2;i<bracketsTable.rows.length;){
+        bracketsTable.deleteRow(2);
+    }
+
+    for(let row = 0;row<selectedYearBrackets.self.length;row++){
+        let bracket = selectedYearBrackets.self[row];
+        let jointBracket = selectedYearBrackets.joint[row];
+        bracketsTable.appendChild(createBracketsRow(bracket, jointBracket));
+    }
+
+    let p1 = person1.value || 'Person 1';
+    let p2 = person2.value || 'Person 2';
+
+    person1Heading.forEach(heading=>{
+        heading.innerHTML = p1;
+    });
+    person2Heading.forEach(heading=>{
+        heading.innerHTML = p2;
+    });
+    
+
+    //filing singly
+    let p1BracketsDue = [];
+    let p2BracketsDue = [];
+    let p1Gross = person1Gross.value || 0;
+    let p2Gross = person2Gross.value || 0;
+    let p1Left = p1Gross;
+    let p2Left = p2Gross;
+
+    for(var i = 2;i<fileSeperate.rows.length;){
+        fileSeperate.deleteRow(2);
+    }
+
+    selectedYearBrackets.self.forEach(bracket=>{
+        let p1Bracket = calcBrack(bracket.amount,bracket.percent,p1BracketsDue,p1Left);
+        p1Left = p1Bracket.left;
+        let p2Bracket = calcBrack(bracket.amount,bracket.percent,p2BracketsDue,p2Left);
+        p2Left = p2Bracket.left;
+        //add row to fileSeperate with bracket.percent, p1Bracket.tax, p2Bracket.tax
+        if(p1Bracket.tax || p2Bracket.tax) {
+            fileSeperate.appendChild(createRow(bracket.percent,p1Bracket.tax,p2Bracket.tax));
+        }        
+    })
+
+    p1Tot.innerHTML = "$"+sum(p1BracketsDue);
+    p2Tot.innerHTML = "$"+sum(p2BracketsDue);
+
+
+    // file jointly
+    let jointBracketsDue = [];
+    let jointLeft = Number(p1Gross) + Number(p2Gross) || 0;
+    let p1JointBracketsDue = [];
+    let p2JointBracketsDue = [];
+    let p1JointLeft = p1Gross || 0;
+    let p2JointLeft = p2Gross || 0;
+
+    for(var i = 2;i<fileJoint.rows.length;){
+        fileJoint.deleteRow(2);
+    }   
+
+    selectedYearBrackets.joint.forEach(bracket=>{
+        let jointBracket = calcBrack(bracket.amount,bracket.percent,jointBracketsDue,jointLeft);
+        jointLeft = jointBracket.left;
+        
+        // calculate split
+        // split by percentage unless one is 0, then just pay the rest
+        // use bracket.amount*splitPercent/100 to get the amount to pay, but adjust if partner has finished
+        let p1Bracket = bracket.amount*slider.value/100;
+        let p2Bracket = bracket.amount*(100-slider.value)/100;
+        if(p1JointLeft < p1Bracket) {
+            p1Bracket = p1JointLeft;
+            p2Bracket = bracket.amount - p1Bracket;
+        } else if(p2JointLeft < p2Bracket) {
+            p2Bracket = p2JointLeft;
+            p1Bracket = bracket.amount - p2Bracket;
+        }
+        let p1JointBracket = calcBrack(p1Bracket,bracket.percent,p1JointBracketsDue,p1JointLeft);
+        p1JointLeft = p1JointBracket.left;
+        let p2JointBracket = calcBrack(p2Bracket,bracket.percent,p2JointBracketsDue,p2JointLeft);
+        p2JointLeft = p2JointBracket.left;
+        //add row to fileSeperate with bracket.percent, p1Bracket.tax, p2Bracket.tax
+        if(jointBracket.tax || p1JointBracket.tax || p2JointBracket.tax) {
+            fileJoint.appendChild(createRow(bracket.percent,jointBracket.tax,p1JointBracket.tax,p2JointBracket.tax));
+        }        
+    });
+
+    p1TotJoint.innerHTML = "$"+Math.round(sum(p1JointBracketsDue)*100)/100;
+    p2TotJoint.innerHTML = "$"+Math.round(sum(p2JointBracketsDue)*100)/100;
+    TotJoint.innerHTML = "$"+Math.round(sum(jointBracketsDue)*100)/100;
+
+};
+
+Object.keys(brackets).reverse().forEach(y=>{
+    year.add(new Option(y));
+});
+year[0].selected = true;
+calculate();
